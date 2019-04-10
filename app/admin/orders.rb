b1a2861ebece0840cd088f21b9981d0d21cd00ba
Matt Millsaps-Brewer <matt@madebylotus.com>
2019-04-10 13:24:37 -0400

    ActiveAdmin Order UI to see issue

---
+++ app/admin/orders.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Order do
+  permit_params payments_attributes: [:id, :amount_cents]
+
+  form do |f|
+    panel 'Payment Methods' do
+      table do
+        thead do
+          th 'ID'
+          th 'Amount Processed'
+        end
+
+        tbody do
+          f.semantic_fields_for :payments do |payments|
+            tr(class: cycle('odd', '')) do
+              td(style: 'display:none') { payments.input :id, as: :hidden }
+              td { payments.object.id }
+              td { payments.text_field :amount_cents }
+            end
+          end
+        end
+      end
+    end
+
+    f.actions
+  end
+end
